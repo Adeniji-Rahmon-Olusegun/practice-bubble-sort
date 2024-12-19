e97cd642b9f8f57ddc57c947a1c50efea75548ab
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-12-19 17:18:32 +0100

    bubble-sort completed

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,17 +2,29 @@
 function bubbleSort(arr) {
 
     // Iterate through the array
+    for (let idx = 0; idx < arr.length - 1; idx++) {
+      let flag = 0;
 
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
+      for (let jdx = 0; jdx < arr.length - 1 - idx; jdx++) {
+        // If the current value is greater than its neighbor to the right
+        if (arr[jdx] > arr[jdx + 1]) {
+          // Swap those values
+          [arr[jdx], arr[jdx + 1]] = [arr[jdx + 1], arr[jdx]];
 
-        // Do not move this console.log
-        console.log(arr.join(","));
+          console.log(arr.join(","));
 
-    // If you get to the end of the array and no swaps have occurred, return
+          flag = 1;
+        }
+      }
 
-    // Otherwise, repeat from the beginning
+      if (flag === 0) break;
 
+    }
+
+    return;
 }
+
+console.log(bubbleSort([2,4,6,8,1,3,5,7,9]));
+
 
 module.exports = bubbleSort;
